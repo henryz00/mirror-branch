8854b9465a6d3ca1305ae7a3d719de1d3bee48c7
Henry Zhang <henryzhang9802@gmail.com>
2020-06-29 21:09:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const sha = context.sha;
 const branch = core.getInput('target-branch', {required: true});
 const force = (core.getInput('force') || 'false').toUpperCase() === 'TRUE'
 
+core.info('SHA - ' + sha)
+
 octokit.git.updateRef({owner, repo, ref: `heads/${branch}`, sha, force}).
   catch(error => {
     core.setFailed(`Failed to update ref: ${error}`);
